2ac59a756033ac00c0d6c1cd8d116dfbd162380a
neelbedarkar <neelanandbedarkar@gmail.com>
2023-03-18 19:36:49 -0500

    Read bucket_name from file

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,18 +1,30 @@
 import boto3
 s3 = boto3.resource('s3')
-bucket_name = 'aws-glue-example-bucket-3.18.2023'
 
-response = s3.create_bucket(
-        Bucket=bucket_name,
-     CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2',
-    },
-    ACL='private'
-)
-print(response)
+with open("bucket_name.txt", "r") as f:
+    bucket_name = f.readline().strip("\n")
 
+if not bucket_name:
+    print("Could not read file. Please create it and bucket name")
+    raise SystemExit
+
+print(bucket_name)
+
+bucket_found = False
 for bucket in s3.buckets.all():
-    print(bucket.name)
+    if bucket.name == bucket_name:
+        bucket_found = True
+
+if not bucket_found:
+    response = s3.create_bucket(
+            Bucket=bucket_name,
+         CreateBucketConfiguration={
+            'LocationConstraint': 'us-east-2',
+        },
+        ACL='private'
+    )
+    print(response)
+
 
 
 
